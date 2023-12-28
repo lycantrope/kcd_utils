814794845374e5fcc799c387ef786b2c26a49a51
Chung-Kuan Chen <b97b01045@gmail.com>
2023-12-28 17:50:28 +0900

    KCD renamer

---
+++ src/lib.rs
@@ -0,0 +1,176 @@
+use std::{
+    fs::File,
+    io::{Read, Write},
+    path::Path,
+};
+
+use anyhow::{Context, Result};
+use deku::{bitvec::Msb0, prelude::*};
+
+pub fn rename_video_hdr<P: AsRef<Path>>(src_hdr: P, dst_hdr: P, prefix: &str) -> Result<()> {
+    // video folder which contains hdr file and videos
+    let dst: &Path = dst_hdr.as_ref().parent().unwrap();
+
+    if !dst.is_dir() {
+        std::fs::create_dir(dst).with_context(|| "Fail to create dst")?;
+    }
+
+    let mut input = File::open(&src_hdr).with_context(|| "Fail to open src")?;
+    let mut src_hdr_buf: Vec<u8> = Vec::new();
+    input.read_to_end(&mut src_hdr_buf)?;
+
+    let (_, mut kcd) =
+        KCDVideoHDR::from_bytes((&src_hdr_buf, 0)).with_context(|| "Fail to parse kcd hdr file")?;
+
+    kcd.rename(prefix, dst)?;
+
+    let mut output = File::create(&dst_hdr).with_context(|| "Fail to create dst_p")?;
+    let kcd_bytes = kcd.to_bytes()?;
+    output.write_all(&kcd_bytes)?;
+
+    Ok(())
+}
+
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+struct KCDVideoHDR {
+    #[deku(bytes_read = "4")]
+    header: Vec<u8>,
+    #[deku(bytes = "4")]
+    count: u32,
+    #[deku(
+        bytes_read = "292 * count",
+        map = "KCDVideoHDR::try_read_video_block",
+        writer = "KCDVideoHDR::try_write_video_block(deku::output, &self.data)"
+    )]
+    data: Vec<VideoBlock>,
+}
+
+impl KCDVideoHDR {
+    fn try_read_video_block(bytes: &[u8]) -> Result<Vec<VideoBlock>, DekuError> {
+        Ok(bytes
+            .chunks(292)
+            .filter_map(|vals| VideoBlock::try_from(vals).ok())
+            .collect())
+    }
+    fn try_write_video_block(
+        output: &mut deku::bitvec::BitVec<u8, Msb0>,
+        data: &Vec<VideoBlock>,
+    ) -> Result<(), DekuError> {
+        for v in data {
+            let _ = v.to_bytes().map(|byte| byte.write(output, ()))?;
+        }
+        Ok(())
+    }
+    fn get_file_ext(&self) -> Option<String> {
+        let mut ext: Vec<_> = self
+            .data
+            .iter()
+            .take(2)
+            .filter_map(
+                |VideoBlock {
+                     _head,
+                     filepath,
+                     _padding,
+                 }| {
+                    let p: &Path = filepath.as_ref();
+                    p.extension().map(|ext| ext.to_string_lossy())
+                },
+            )
+            .collect();
+        ext.pop().map(|v| v.to_string())
+    }
+
+    fn rename<P: AsRef<Path>>(&mut self, prefix: &str, folder: P) -> Result<()> {
+        let ext = self.get_file_ext().unwrap_or("avi".to_string());
+
+        let src_folder: &Path = folder.as_ref();
+
+        self.data.iter_mut().enumerate().try_for_each(|(i, block)| {
+            let new_name = format!("{}{}.{}", prefix, i + 1, ext);
+            block.filepath = format!("{}\\{}", prefix, &new_name);
+            if let Some(src) = block
+                .filepath
+                .split('\\')
+                .nth(1)
+                .map(|x| src_folder.join(x))
+            {
+                if src.is_file() {
+                    let dst = src.with_file_name(&new_name);
+                    std::fs::rename(&src, dst).with_context(|| "Fail to rename video")?;
+                }
+            }
+            Ok(())
+        })
+    }
+}
+
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+struct VideoBlock {
+    #[deku(bytes_read = "16")]
+    _head: Vec<u8>,
+    #[deku(
+        bytes_read = "256",
+        map = "VideoBlock::map_to_string",
+        writer = "VideoBlock::write_to_string(deku::output, &self.filepath)"
+    )]
+    filepath: String,
+    #[deku(bytes_read = "20")]
+    _padding: Vec<u8>,
+}
+
+impl VideoBlock {
+    fn map_to_string(data: &[u8]) -> Result<String, DekuError> {
+        let out: String = data
+            .iter()
+            .cloned()
+            .filter_map(|v| char::try_from(v).ok())
+            .collect();
+        Ok(out.trim_matches(char::from(0)).to_string())
+    }
+    fn write_to_string(
+        output: &mut deku::bitvec::BitVec<u8, Msb0>,
+        field: &str,
+    ) -> Result<(), DekuError> {
+        let val = field.as_bytes();
+        let n = val.len();
+        val.write(output, ())?;
+        if n < 256 {
+            let padding = vec![0u8; 256 - n];
+            padding.write(output, ())?;
+        }
+        Ok(())
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{
+        fs::File,
+        io::{Read, Write},
+    };
+
+    #[test]
+    fn read_hdr() -> anyhow::Result<()> {
+        let p = "/Users/chungkuanchen/Projects/kcd_rename/16-1_basal_nos-m4_230925-230927_3487-3488.0001.hdr";
+        let mut file = File::open(p)?;
+        let mut data: Vec<u8> = Vec::new();
+        file.read_to_end(&mut data)?;
+
+        let (_, kcd) = KCDVideoHDR::from_bytes((&data, 0))?;
+        Ok(())
+    }
+    #[test]
+    fn write_to_hdr() -> anyhow::Result<()> {
+        let p = "/Users/chungkuanchen/Projects/kcd_rename/16-1_basal_nos-m4_230925-230927_3487-3488.0001.hdr";
+        let mut file = File::open(p)?;
+        let mut original: Vec<u8> = Vec::new();
+        file.read_to_end(&mut original)?;
+
+        let (_, kcd) = KCDVideoHDR::from_bytes((&original, 0))?;
+        let mut out = File::create("./test_out.hdr")?;
+        let kcd_bytes = kcd.to_bytes()?;
+        out.write_all(&kcd_bytes)?;
+        assert_eq!(kcd_bytes, original);
+        Ok(())
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,127 @@
+use anyhow::{bail, Context, Result};
+use clap::error::ErrorKind;
+use clap::{CommandFactory, Parser};
+use patternscan::scan;
+use std::fs::File;
+use std::io::BufWriter;
+use std::io::{BufReader, Read, Write};
+use std::path::{Path, PathBuf};
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(name = "kcd_rename")]
+#[command(version = "0.1.0")]
+#[command(author = "Chung-Kuan Chen <b97b01045@gmail.com>")]
+#[command(about = "A CLI Tool to rename KISSEICOMTEC Raw Data (KCD) file", long_about = None)]
+struct Cli {
+    /// Name of the person to greet
+    #[arg(short, long, value_name = "KCD FILE")]
+    input: PathBuf,
+
+    /// Number of times to greet
+    #[arg(short, long, value_name = "OUTPUT")]
+    output: PathBuf,
+}
+
+fn get_kcrmovie_position<P: AsRef<Path>>(p: P) -> Result<usize> {
+    // let mut file = File::open(p)?;
+    // let mut buf = Vec::new();
+    // file.read_to_end(buf.as_mut())?;
+    // let reader = Cursor::new(buf);
+
+    let file = File::open(p)?;
+    let reader = BufReader::with_capacity(1024, file);
+
+    let pattern = "4B 43 52 4D 4F 56 49 45";
+    // let pattern = "45 49 56 4f 4D 52 43 4B";
+
+    let res = dbg!(scan(reader, pattern));
+    match res {
+        Ok(pos) if pos.len() == 1 => Ok(pos[0]),
+        _ => bail!("Invalid KCD file. (no `KCRMOIVE` or multiple kcd found)"),
+    }
+}
+
+fn change_kcrmovie_text<P: AsRef<Path>>(input: P, output: P, pos: usize) -> Result<()> {
+    let input_p: &Path = input.as_ref();
+    let output_p: &Path = output.as_ref();
+
+    let file_stem = output_p
+        .file_stem()
+        .with_context(|| format!("output is not a valid name: {}", output_p.display()))?
+        .to_string_lossy();
+
+    let mut buf: Vec<u8> = Vec::new();
+    let mut file = File::open(input_p).with_context(|| "Fail to open input file")?;
+    file.read_to_end(&mut buf)?;
+    let total: usize = buf.len();
+    let mut writer = BufWriter::new(File::create(output_p)?);
+
+    let _ = writer.write(&buf[0..total.min(pos + 16)])?;
+
+    let hdr_tag: String = format!("{}\\{}.hdr", file_stem, file_stem);
+    let hdr_buf = hdr_tag.as_bytes();
+    if hdr_buf.len() > 256 {
+        bail!("output name is too long, please reduce to < 120 charaters");
+    }
+    let padding = vec![0u8; 256 - hdr_buf.len()];
+    writer.write_all(hdr_buf)?;
+    writer.write_all(&padding)?;
+    writer
+        .write(&buf[(pos + 256 + 16).min(total)..total])
+        .with_context(|| "Fail to write remaining bytes")?;
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+    let mut cmd = Cli::command();
+    if !cli.input.is_file() {
+        cmd.error(ErrorKind::Io, "Input is not a file").exit()
+    }
+
+    let file_stem = cli.input.file_stem().unwrap().to_string_lossy();
+    let prefix = cli.output.file_stem().unwrap().to_string_lossy();
+
+    let video_folder: PathBuf = cli.input.with_file_name(file_stem.as_ref());
+
+    let video_hdr: PathBuf = video_folder.join(format!("{}.hdr", file_stem));
+    if !video_hdr.exists() {
+        println!("Could not find video folder or video.hdr file!");
+        println!("Only copy file to the output");
+        if !cli.output.exists() {
+            let _ = std::fs::copy(cli.input, cli.output)?;
+        } else {
+            cmd.error(ErrorKind::Io, "Target file existed! Aborted the copy")
+                .exit();
+        }
+        return Ok(());
+    }
+
+    let pos = if let Ok(pos) = get_kcrmovie_position(cli.input.as_path()) {
+        pos
+    } else {
+        cmd.error(ErrorKind::Io, "Fail to retrieve position").exit();
+    };
+
+    change_kcrmovie_text(&cli.input, &cli.output, pos)?;
+    let dst_hdr = video_folder
+        .with_file_name(prefix.as_ref())
+        .join(format!("{}.hdr", prefix));
+    println!("{:?}", dst_hdr.display());
+    match kcd_rename::rename_video_hdr(video_hdr, dst_hdr, &prefix) {
+        Err(e) => cmd.error(ErrorKind::Io, format!("{:?}", e)).exit(),
+        _ => Ok(()),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_kcrmovie_position() -> Result<()> {
+        let pos = get_kcrmovie_position("./short_test.kcd")?;
+
+        Ok(())
+    }
+}
